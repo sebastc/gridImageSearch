3d9b3c17ff0d4f5e19fd3e751dd319db0bd79c9a
sebastc <chsebast@gmail.com>
2014-09-13 17:33:15 -0700

    Add UI for settings and update settings opbject.

--- gridImageSearch/src/com/yahoo/sebastc/gridimagesearch/Settings.java
+++ gridImageSearch/src/com/yahoo/sebastc/gridimagesearch/Settings.java
@@ -8,7 +8,20 @@ public class Settings implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -7654206045766945726L;
-	
-	
+	public Size imageSize = Size.ALL;
+	public ImageColor imageColor = ImageColor.ALL;
+	public ImageType imageType = ImageType.ALL;
+	public String imageSite = null;
 
+	public enum Size {
+		ALL, SMALL, MEDIUM, LARGE, EXTRA_LARGE
+	}
+
+	public enum ImageColor {
+		ALL, BLACK, BLUE, BROWN, GRAY, GREEN
+	}
+
+	public enum ImageType {
+		ALL, FACES, PHOTO, CLIP_ART, LINE_ART
+	}
 }
--- gridImageSearch/src/com/yahoo/sebastc/gridimagesearch/SettingsActivity.java
+++ gridImageSearch/src/com/yahoo/sebastc/gridimagesearch/SettingsActivity.java
@@ -3,8 +3,19 @@ package com.yahoo.sebastc.gridimagesearch;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.yahoo.sebastc.gridimagesearch.Settings.ImageColor;
+import com.yahoo.sebastc.gridimagesearch.Settings.ImageType;
+import com.yahoo.sebastc.gridimagesearch.Settings.Size;
 
 public class SettingsActivity extends Activity {
 	private Settings settings;
@@ -14,6 +25,75 @@ public class SettingsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 		settings = (Settings) getIntent().getSerializableExtra("settings");
+		setupSpinner(R.id.spinnerSize, settings.imageSize, new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				settings.imageSize = Size.values()[position];
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				settings.imageSize = Size.ALL;
+			}
+		});
+		setupSpinner(R.id.spinnerColor, settings.imageColor, new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				settings.imageColor = ImageColor.values()[position];
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				settings.imageColor = ImageColor.ALL;
+			}
+		});
+		setupSpinner(R.id.spinnerType, settings.imageType, new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				settings.imageType = ImageType.values()[position];
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				settings.imageSize = Size.ALL;
+			}
+		});
+		setupTextField(R.id.etSite, settings.imageSite, new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				settings.imageSite = s.toString();
+			}
+		});
+		
+	}
+
+	private void setupSpinner(int id, Enum<?> value, OnItemSelectedListener listener) {
+		Spinner spinner = (Spinner) findViewById(id);
+		spinner.setSelection(value.ordinal());
+		spinner.setOnItemSelectedListener(listener);
+	}
+	private void setupTextField(int id, String value, TextWatcher listener) {
+		EditText spinner = (EditText) findViewById(id);
+		if(value !=null && value.trim().isEmpty())
+			value = null;
+		spinner.setText(value);
+		spinner.addTextChangedListener(listener);
 	}
 
 	@Override
